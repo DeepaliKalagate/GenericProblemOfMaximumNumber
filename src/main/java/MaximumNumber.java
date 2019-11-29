c436311b8c69071028789ba732736534d212c779
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-29 13:09:57 +0530

    Refactored code and made generic class with parameterised constructor

--- src/main/java/MaximumNumber.java
+++ src/main/java/MaximumNumber.java
@@ -1,5 +1,16 @@
-public class MaximumNumber
+public class MaximumNumber<E>
 {
+
+    E a;
+    E b;
+    E c;
+
+    public MaximumNumber(E a, E b, E c)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
     public static  <E extends Object> E findMaximum(E a,E b,E c)
     {
         E max=a;
